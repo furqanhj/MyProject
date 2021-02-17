f4c6982f289557f66d579136cdc293f408697fab
= <=>
2021-02-17 03:47:22 -0600

    Practicing Commenting

--- src/UsingScanners/project1.java
+++ src/UsingScanners/project1.java
@@ -2,11 +2,26 @@ package UsingScanners;
 
 import java.util.Scanner;
 
+/**
+ * This is just a test project to practice my commenting.
+ * Here I am practicing Documentation Commenting.
+ * This same module is already done in WDE_Core_Java_2021 Project.
+ * @Furqan
+ * @02/16/2021
+ */
 public class project1 {
+
+    // This is a test for single-line commenting.
 
     static String firstName;
     static String lastName;
     static int age;
+
+/*
+This is an example of a mutli-line commenting. this way whatever are between these forward slashes
+will all be just comments.
+No matter how many times we press enter button, it will still remain as comments
+ */
 
     static String printName (){
         Scanner scanner = new Scanner(System.in);
