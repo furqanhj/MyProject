d91f508b4534a866bd44dcf227daaa839607a096
= <=>
2021-02-17 02:39:13 -0600

    First commit: adding MyProject to the github repository

---
+++ src/UsingScanners/project1.java
@@ -0,0 +1,31 @@
+package UsingScanners;
+
+import java.util.Scanner;
+
+public class project1 {
+
+    static String firstName;
+    static String lastName;
+    static int age;
+
+    static String printName (){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first name:");
+        firstName = scanner.nextLine();
+
+        System.out.println("Enter last name:");
+        lastName = scanner.nextLine();
+
+        System.out.println("Enter your age");
+        String ageInWords = String.valueOf(age);
+        ageInWords = scanner.nextLine();
+
+        return firstName + " " + lastName + " " + ageInWords;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The entered name is: " + printName());
+    }
+}
