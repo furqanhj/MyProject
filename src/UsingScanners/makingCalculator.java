e90502bba075e2de2714da625253f0d2c89bda67
= <=>
2021-02-17 03:36:21 -0600

    Modified with a new Calculator class

---
+++ src/UsingScanners/makingCalculator.java
@@ -0,0 +1,42 @@
+package UsingScanners;
+
+import java.util.Scanner;
+
+public class makingCalculator {
+    static double num1;
+    static double num2;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        num1 = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Enter the second number");
+        num2 = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("By adding both your numbers, the answer is: " + doAddition());
+        System.out.println("By subtracting both your numbers, the answer is: " + doSubtraction());
+        System.out.println("By multiplying both your numbers, the answer is: " + doMultiplication());
+        System.out.println("By dividing both your numbers, the answer is: " + doDivision());
+
+        scanner.close();
+    }
+
+    public static double doAddition(){
+        return num1 + num2;
+    }
+
+    public static double doSubtraction(){
+        return num1 - num2;
+    }
+
+    public static double doMultiplication(){
+        return num1 * num2;
+    }
+
+    public static double doDivision(){
+        return num1 / num2;
+    }
+
+}
