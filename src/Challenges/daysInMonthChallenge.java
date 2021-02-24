6913a9ab6bd6d20991024c5ef44f9e6b60f7669a
= <=>
2021-02-24 02:24:46 -0600

    modified DaysInmonth Challenge

--- src/Challenges/daysInMonthChallenge.java
+++ src/Challenges/daysInMonthChallenge.java
@@ -5,69 +5,29 @@ public class daysInMonthChallenge {
     public static void main(String[] args) {
 
         daysInMonthChallenge printDays = new daysInMonthChallenge();
-        System.out.println(printDays.daysOfMonth("Jan"));
+        System.out.println(printDays.daysOfMonth("February"));
     }
-
 
     public int daysOfMonth(String nameOfMonth) {
 
         int daysInNum = -1;
 
         switch (nameOfMonth) {
-            case "Jan":
+            case "January": case "March": case "May": case "July": case "August": case "October": case "December":
                 daysInNum = 31;
                 break;
-            case "Feb":
+
+            case "February":
                 daysInNum = 28;
                 break;
 
-            case "Mar":
-                daysInNum = 31;
-                break;
-
-            case "Apr":
-                daysInNum = 30;
-                break;
-
-            case "May":
-                daysInNum = 31;
-                break;
-
-            case "Jun":
-                daysInNum = 30;
-                break;
-
-            case "Jul":
-                daysInNum = 31;
-                break;
-
-            case "Aug":
-                daysInNum = 30;
-                break;
-
-            case "Sept":
-                daysInNum = 31;
-                break;
-
-            case "Oct":
-                daysInNum = 30;
-                break;
-
-            case "Nov":
-                daysInNum = 31;
-                break;
-
-            case "Dec":
+            case "April": case "June": case "September": case "November":
                 daysInNum = 31;
                 break;
 
             default:
                 return daysInNum;
-
         }
-
         return daysInNum;
     }
-
-
 }
