d39f1206cedda731f7bf2a9c0f7ca3d8c65a1aa6
= <=>
2021-02-24 02:19:00 -0600

    Finished ControlFlowChallenge

--- src/Challenges/ControlFlowChallenge.java
+++ src/Challenges/ControlFlowChallenge.java
@@ -1,16 +1,34 @@
 package Challenges;
 
 public class ControlFlowChallenge {
+// Write methods to print out even and odd numbers:
 
     public static void main(String[] args) {
+        getEvenNumbers();
+        getOddNumbers();
 
-        for (int i = 0; i <= 100 ; i++) {
-            
-        }
-        
     }
-  
+
+    public static void getEvenNumbers() {
+        System.out.println("The even numbers are as follows: ");
+        for (int i = 1; i < 101; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+
+    public static void getOddNumbers() {
+        System.out.println("\nThe odd numbers are as follows: ");
+        for (int i = 1; i < 101; i++) {
+            if (i % 2 == 1) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
 }
+  
+
 
 
 
